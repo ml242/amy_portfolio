702f24e2d194f32c54bedb2add08ada2baa83a34
Matt Lucas <mattlucas@gmail.com>
2014-02-15 18:41:55 -0500

    updating this old site for refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amygironda::Application.config.secret_token = '14e504a0b2ee23ac4c42822097cf86a973196503725525fb32351be08779c60185ad65282d73d9e710ed5db6a31f2560b4be82ad4f793afdc6a8b67e216d56de'
